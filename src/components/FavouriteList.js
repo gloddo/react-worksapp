54d3e0e8ae1bcc5790385e2145749f4b4fc5a7a2
Mik23mik <46992470+Mik23mik@users.noreply.github.com>
2019-03-25 16:42:27 +0100

    favourite navbar definitiva!

--- src/components/FavouriteList.js
+++ src/components/FavouriteList.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import ProfilePic from "./ProfilePic"
 
-<<<<<<< HEAD
 export default class FavouriteList extends Component {
     render() {
         return this.props.chats.map((el, i) => {
@@ -11,17 +10,4 @@ export default class FavouriteList extends Component {
             
         })
     }
-=======
-export default class ChatList extends Component {
-    render() {
-        let array = [0,1,2,3,4,5];
-        return (
-            array.forEach(()=>{
-                <ProfilePic img={this.props.img} state={this.props.state} />
-            })
-        )
-    }
-    
-
->>>>>>> 4f58db68c3ea8b1ef4bc1fbc50ccb35b6376c345
 }
--- src/components/FavouriteNavbar.js
+++ src/components/FavouriteNavbar.js
@@ -1,11 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-<<<<<<< HEAD
 import { FaRegStar, FaAngleRight } from "react-icons/fa"
 import FavouriteList from "./FavouriteList"
-=======
-import { FaRegStar } from "react-icons/fa"
->>>>>>> 4f58db68c3ea8b1ef4bc1fbc50ccb35b6376c345
 
 
 export default class FavouriteNavbar extends Component {
@@ -14,11 +10,8 @@ export default class FavouriteNavbar extends Component {
             <div>
                 <FaRegStar />
                 <div>
-<<<<<<< HEAD
                     <FavouriteList chats={this.props.chats} />
-=======
-                    <FavouriteList img={this.props.img} state={this.props.state} />
->>>>>>> 4f58db68c3ea8b1ef4bc1fbc50ccb35b6376c345
+
                 </div>
                 <FaAngleRight />
             </div>

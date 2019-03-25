b9b1ef91ab30b500346dffe3ed1bc0551b672b01
Joe Makkio <makkio94@gmail.com>
2019-03-25 14:59:04 +0100

    Update ChatList.js

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
-import ChatListEntry from "./ChatListEntry"
+import ChatListEntry from "./ChatListEntry";
 
 export default class ChatList extends Component {
-    render() {
-        return (
-            this.props.chats.map((el, i)=>{
-                return <ChatListEntry key={i} img={el.img} name={el.name} surname={el.surname} role={el.role} date={el.date} notify={el.notify}/>
-            })
-        )
-    }
-    
-
+  render() {
+    return this.props.chats.map((el, i) => {
+      return (
+        <ChatListEntry
+          key={i}
+          img={el.img}
+          name={el.name}
+          surname={el.surname}
+          role={el.role}
+          date={el.date}
+          notify={el.notify}
+        />
+      );
+    });
+  }
 }

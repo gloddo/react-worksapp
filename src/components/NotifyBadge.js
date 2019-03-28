efcb2374f8f62f1893b8c210af4b3bf2dd20192e
Joe Makkio <makkio94@gmail.com>
2019-03-28 14:21:32 +0100

    Update NotifyBadge.js

--- src/components/NotifyBadge.js
+++ src/components/NotifyBadge.js
@@ -3,10 +3,11 @@ import React, { Component } from "react";
 export default class Date extends Component {
   render() {
     return (
+      this.props.notify !== 0 && (
         <div className="notify-badge">
-          {this.props.notify && (this.props.notify > 99 ? "99+" : this.props.notify)}
+          {this.props.notify > 99 ? "99+" : this.props.notify}
         </div>
-      
+      )
     );
   }
 }

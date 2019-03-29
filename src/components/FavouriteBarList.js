20d0bc5d29138f8ff2c7d78a22887792f4f1d6e4
Joe Makkio <makkio94@gmail.com>
2019-03-29 14:31:06 +0100

    Update FavouriteBarList.js

--- src/components/FavouriteBarList.js
+++ src/components/FavouriteBarList.js
@@ -4,7 +4,7 @@ import ProfilePic from "./ProfilePic";
 export default class FavouriteBarList extends Component {
   render() {
     return this.props.favourites.map((el, i) => {
-      if(el.favs) return <li key={i}><ProfilePic key={i} img={el.img} click={this.props.click} state={el.state} /></li>
+      return (el.favs && <li key={i}><ProfilePic key={i} img={el.img} click={this.props.click} state={el.state} /></li>)
     });
   }
 }

3599c1739bcade93decb5336509af3aa797da6d1
Joe Makkio <makkio94@gmail.com>
2019-04-19 16:30:18 +0200

    Update EditProfile.js

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import ProfilePic from "./ProfilePic";
 import "./EditProfile.css";
 export default class EditProfile extends Component {
